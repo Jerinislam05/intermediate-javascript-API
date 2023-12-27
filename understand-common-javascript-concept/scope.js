185ceb224acf8541931bf4a2ea990bdea60d2dcf
Jerinislam05 <jerinislam05@gmail.com>
2023-12-27 23:40:14 +0000

    scope and hoisting

---
+++ understand-common-javascript-concept/scope.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+    const total = a+b;
+    // console.log(a, b);
+    return total;
+    if(b>5) {
+        const sum = 25 + a + b;
+    } 
+    else {
+        const sum = 5 + a + b;
+        var current = sum;
+    }
+    console.log(current);
+    return total;
+    // console.log(sum);
+}
+add(5, 3);
+// console.log(a, b); 
+//function er vetore console.log korte hobe baire dile error hobe, function er vetore kono variable declare korle seta function er baire paowa jabena
+// second bracket er moddhe thaklei seta scope 
