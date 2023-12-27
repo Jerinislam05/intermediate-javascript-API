35bc34864441e052637d1552a4571bf555eebf9b
Jerinislam05 <jerinislam05@gmail.com>
2023-12-27 20:09:50 +0000

    double equal triple equal

---
+++ understand-common-javascript-concept/triple.js
@@ -0,0 +1,13 @@
+// always use ===
+// equal comparison doesn't work for non-premitive
+const first = [];
+const second = first;
+if (first === second) { // double equal only man check kore r triple equal man value 2ta e check kore tai always double equal use kora uchit 99% khetre
+    console.log('values are equal');
+} else {
+    console.log('values are different');
+}
+// type coercion
+// type conversion
+// type casting
+// avoid double equal use triple equal
