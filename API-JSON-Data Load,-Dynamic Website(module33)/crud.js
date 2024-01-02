b38fcf04f6edf7a25d78ff74148bf1beba78c15a
Jerinislam05 <jerinislam05@gmail.com>
2024-01-02 13:05:46 +0000

    REST API Methods, CRUD

---
+++ API-JSON-Data Load,-Dynamic Website(module33)/crud.js
@@ -0,0 +1,14 @@
+// CRUD => CREATE, READ, UPDATE, DELETE
+
+/* REST API Methods
+--------------------------
+GET- kono information recieve korte jekono API source theke 
+
+PUT- kono kichu change korte use hoy, jodi kono kichu nao thake oi place e put create kore set kore, update kore
+
+PATCH- kono jinish update korte
+
+POST- notun kichu add korte, create korte
+
+DELETE- kisu delete korte
+*/
