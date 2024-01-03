210ef4d72216de068c469cb2a22db58b943fb41c
Jerinislam05 <jerinislam05@gmail.com>
2024-01-03 11:47:17 +0000

    debug API

--- API-JSON-Data Load,-Dynamic Website(module33)/post.js
+++ API-JSON-Data Load,-Dynamic Website(module33)/post.js
@@ -1,22 +1,24 @@
 function loadPosts() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(res => res.json())
-    .then(data => displayPosts(data))
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    .then((data) => displayPosts(data));
 }
 
 function displayPosts(posts) {
-    const postContainer = document.getElementById('posts-container');
-    for (const post of posts) {
-        const postDiv = document.createElement('div');
-        postDiv.classList.add('post')
-        console.log(post);
-        postDiv.innerHTML = `
+  const postContainer = document.getElementById("posts-container");
+  for (const post of posts) {
+    const postDiv = document.createElement("div");
+    postDiv.classList.add("post");
+    console.log(post);
+    postDiv.innerHTML = `
             <h4>User-${post.userId}</h4>
             <h5>Post:title${post.title}</h5>
             <p>Post Description:${post.body}</p>
         `;
-        postContainer.appendChild(postDiv);
-    }
+    postContainer.appendChild(postDiv);
+  }
 }
 
 loadPosts();
+
+
