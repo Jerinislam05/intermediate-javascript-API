d0e717acff6e04b7f0d02a75f9b8beb02d3a96d4
Jerinislam05 <jerinislam05@gmail.com>
2024-01-04 17:35:07 +0000

    Search and display search result

--- API-recap-with-Phone-Hunter(module34)/phone.js
+++ API-recap-with-Phone-Hunter(module34)/phone.js
@@ -1,7 +1,5 @@
-const loadPhone = async () => {
-  const res = await fetch(
-    "https://openapi.programming-hero.com/api/phones?search=iphone"
-  );
+const loadPhone = async (searchText) => {
+  const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
   const data = await res.json();
   const phones = data.data;
   // console.log(phones);
@@ -11,13 +9,15 @@ const loadPhone = async () => {
 const displayPhones = (phones) => {
   // console.log(phones);
   //1. get element by id
-  const phoneContainer = document.getElementById('phone-container')
+  const phoneContainer = document.getElementById('phone-container');
+//clear the container cards
+  phoneContainer.textContent = '';
 
   phones.forEach((phone) => {
     console.log(phone);
     //2. create a div
     const phoneCard = document.createElement("div");
-    phoneCard.classList = `card w-96 bg-gray-100 shadow-xl`;
+    phoneCard.classList = `card p-4 bg-gray-100 shadow-xl`;
     //3. set innerhtml
     phoneCard.innerHTML = `
     <figure>
@@ -36,4 +36,12 @@ const displayPhones = (phones) => {
   });
 };
 
-loadPhone();
+// handle search button
+const handleSearchButton = () => {
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    console.log(searchText);
+    loadPhone(searchText);
+}
+
+// loadPhone();
