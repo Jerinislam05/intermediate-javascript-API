34e9809f1679de2af6da9bc63aebacdb0bd66ab0
Jerinislam05 <jerinislam05@gmail.com>
2024-01-05 17:18:57 +0000

    Nested object data

--- API-recap-with-Phone-Hunter(module34)/phone.js
+++ API-recap-with-Phone-Hunter(module34)/phone.js
@@ -1,4 +1,4 @@
-const loadPhone = async (searchText) => {
+const loadPhone = async (searchText='13') => {
   const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
   const data = await res.json();
   const phones = data.data;
@@ -29,7 +29,7 @@ if(phones.length > 12) {
     //2. create a div
     const phoneCard = document.createElement("div");
     phoneCard.classList = `card p-4 bg-gray-100 shadow-xl`;
-    //3. set innerhtml
+    //3. set inner html
     phoneCard.innerHTML = `
     <figure>
         <img src="${phone.image}" alt="Shoes"/>
@@ -38,7 +38,7 @@ if(phones.length > 12) {
         <h2 class="card-title">${phone.phone_name}</h2>
         <p>If a dog chews shoes whose shoes does he choose?</p>
         <div class="card-actions justify-center">
-        <button onclick="handleShowDetails('${phone.slug}')" class="btn btn-primary">show details</button>
+        <button onclick="handleShowDetails('${phone.slug}');show_details_modal.showModal()" class="btn btn-primary">show details</button>
         </div>
     </div>
         `;
@@ -53,11 +53,34 @@ toggleLoadingSpinner(false)
 
 // 
 const handleShowDetails = async (id) => {
-    console.log('clicked on show details', id);
+    // console.log('clicked on show details', id);
     // load individual/single phone data
     const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
     const data = await res.json();
-    console.log(data);
+    const phone = data.data;
+
+    showPhoneDetails(phone);
+}
+
+const showPhoneDetails = (phone) => {
+    console.log(phone);
+    const phoneName = document.getElementById('show-detail-phone-name');
+    phoneName.innerText = phone.name;
+
+    const showDetailContainer = document.getElementById('show-detail-container');
+    showDetailContainer.innerHTML = `
+        <img src="${phone.image}" alt="" />
+        <p>Brand: ${phone.brand}</p>
+        <p>Name: ${phone.name}</p>
+        <p><span> Storage:</span>${phone?.mainFeatures?.storage}</p>
+        <p><span> Display:</span>${phone?.mainFeatures?.displaySize}</p>
+        <p><span> Release Date:</span>${phone?.mainFeatures?.releaseDate || 'No Date'}</p>
+        <p><span> GPS:</span>${phone?.others?.GPS || 'No GPS'}</p>
+    `
+
+
+    // show the modal
+    show_details_modal.showModal()
 }
 
 // handle search button
@@ -65,7 +88,7 @@ const handleSearchButton = () => {
     toggleLoadingSpinner(true);
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
-    console.log(searchText);
+    // console.log(searchText);
     loadPhone(searchText);
 }
 
@@ -86,4 +109,4 @@ const toggleLoadingSpinner = (isLoading) => {
     }
 }
 
-// loadPhone();
+loadPhone();
