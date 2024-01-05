6b55d560220cec44f00f546bdb299939aba2a018
Jerinislam05 <jerinislam05@gmail.com>
2024-01-05 15:58:35 +0000

    dynamic API, show details button

--- API-recap-with-Phone-Hunter(module34)/phone.js
+++ API-recap-with-Phone-Hunter(module34)/phone.js
@@ -37,8 +37,8 @@ if(phones.length > 12) {
     <div class="card-body">
         <h2 class="card-title">${phone.phone_name}</h2>
         <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
-        <button class="btn btn-primary">Buy Now</button>
+        <div class="card-actions justify-center">
+        <button onclick="handleShowDetails('${phone.slug}')" class="btn btn-primary">show details</button>
         </div>
     </div>
         `;
@@ -50,6 +50,15 @@ if(phones.length > 12) {
 toggleLoadingSpinner(false)
 
 };
+
+// 
+const handleShowDetails = async (id) => {
+    console.log('clicked on show details', id);
+    // load individual/single phone data
+    const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+    const data = await res.json();
+    console.log(data);
+}
 
 // handle search button
 const handleSearchButton = () => {
