9e7aad49d98d81d94c0c3527d21661e3ac57b736
Jerinislam05 <jerinislam05@gmail.com>
2024-01-06 11:37:59 +0000

    Context and call stack

---
+++ more-about-js(35)/execution-context-call-stack.js
@@ -0,0 +1,17 @@
+// Execution Context
+
+/* 
+1. An execution context is an environment.
+
+2. inside the execution context a piece of JavaScript code gets executed
+
+3. Variable, parameters and information related to the piece of code get stored in the execution context.
+
+4. There are two kinds of execution context in JavaScript:
+** Global Execution Context(GEC) and Function Execution Context(FEC).
+
+5. The execution context is created in two phases: 
+->Creation phase and ->Execution phase.
+
+6.
+*/
