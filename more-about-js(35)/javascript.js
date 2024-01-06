12e2ec5575dcd8dfe7a417e004561721a820ec52
Jerinislam05 <jerinislam05@gmail.com>
2024-01-06 09:32:07 +0000

    about javascript

---
+++ more-about-js(35)/javascript.js
@@ -0,0 +1,15 @@
+// what is javascript?
+/* 
+1. Javascript is highly abstracted programming language. [High abstraction means => hiding the implementation details, showing only functionality to the users, memory allocation, garbage collection, understandability and maintainability]
+
+2. Javascript is a JIT(Just-In-Time) compiled language. [JIT means => Just-In-Time compiler converts the entire code into machine code and execute them immediately]
+
+3. JavaScript is a multi-paradigm programming language.[Paradigm means => code structure that will determine the style or a way of programming. For example, procedural programming(past), object-oriented programming(nowadays), functional programming]
+
+4. JavaScript is a proto-type based programming language. [proto-type means => everything is object except the primitive data. A proto-type object is an object used as a template from which to get the initial properties for a new object. Proto-type is a blue print]
+
+5. JavaScript is dynamically typed
+
+
+** JavaScript is a JIT compiled, multi-paradigm, prototype-based, dynamically typed, high abstracted programming language.
+*/
