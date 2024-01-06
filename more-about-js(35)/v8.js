742d0b0b011d1f2c68a0ddcab61a9dfe2d163c31
Jerinislam05 <jerinislam05@gmail.com>
2024-01-06 10:31:53 +0000

    V8 Engine

---
+++ more-about-js(35)/v8.js
@@ -0,0 +1,7 @@
+// V8 Engine JavaScript
+
+/* 
+1. v8 is google's open source high-performance JavaScript and WebAssembly engine, written in C++. [v8 engine javascript file k parse kore execute kore]
+
+2. V8 => js source code-> v8 engine-> parsing-> abstract syntax tree-> byte code-> machine code-> run code on the CPU. [V8 diye javascript, nodeJS chole]
+*/
